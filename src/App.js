085ab360eb80bb00395d21c3ebb76a82c40b6db7
nawaf-vp <nawafvp.dev@gmail.com>
2022-12-24 08:46:47 +0530

    movie app which shows the relevant movies which according to search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies([]);
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);/* setMovies function is search movies from the api */
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <h1>MovieLand</h1>
+{/* movie search container */}
+      <div className="search">
+        <input
+          value={searchTerm} /* set value as searchterm state */
+          onChange={(e) => setSearchTerm(e.target.value)} /* e is a event */
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}/* when clicking the search icon .calls searchMovies and
+                                                       props as search term */
+        />
+      </div>
+
+{/* movies showing container */}
+      {movies?.length > 0 ? (
+        <div className="container">{/* loads when movies search  */} 
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">{/* show when no movies is to show  */}
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+
+      <div>
+        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+                    {/* if the poster pic is not available it shows  a 400*400 placeholder */}
+      </div>
+
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
+ 
